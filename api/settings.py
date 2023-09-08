3792a6d6b7ddde3e0d3ce58a190d1f007690e94a
Michi Devall <85964463+michidevall@users.noreply.github.com>
2023-09-08 11:18:00 -0600

    settings

--- api/settings.py
+++ api/settings.py
@@ -15,6 +15,8 @@ import django_heroku
 import dotenv
 import dj_database_url
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
